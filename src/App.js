103dc886f502adc70d5a5c6cc32c1a051ba506cf
Marko Lukić <lukic@netsearch.cz>
2021-04-07 22:44:46 +0200

    Import the Row component & implement it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import Row from './components/Row/Row.jsx'
+import apiClient from './modules/apiClient'
 
 function App() {
   return (
     <div className="container">
       <h1>Hello 24i!</h1>
+      <Row title="Popular Movies" getURL={apiClient.getMovieList} />
+      <Row title="Popular Series" getURL={apiClient.getTVList} />
+      <Row title="Family" getURL={apiClient.getFamilyMovies} />
+      <Row title="Documentaries" getURL={apiClient.getDocumentaryMovies} />
+
+
     </div>
   );
 }
