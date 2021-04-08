77403dae550eaa9f6fee30c266b348c3f92f6fca
Marko Lukić <lukic@netsearch.cz>
2021-04-08 19:42:57 +0200

    Import navigation into the App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import Homepage from './pages/homepage';
+import Navigation from './components/Navigation'
 
 import "./App.scss"
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <Router>
     <div className="App">
+      <Navigation />
       <Route exact path="/" render={ () => <Redirect to="/homepage" /> }/>
       <Route path="/homepage" component={Homepage} />
       {/* <Route path="/streaming" component={StreamingPage} /> */}
