45f417f1df9f14af534254c224465a10ac5a0f89
Marko Lukić <lukic@netsearch.cz>
2021-04-08 19:43:10 +0200

    Navigation cleanup

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap/Navbar'
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import { Nav, Container } from 'react-bootstrap'
 
 function Navigation() {
     return (
         <div>
         <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand href='/homepage'>24i Interview</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href='/homepage'>Home</Nav.Link>
         </Nav>
         {/* <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -6,7 +6,6 @@ import apiClient from "../modules/apiClient";
 function Homepage() {
   return (
     <div className="wrapper">
-      <h1 className="header">Hello 24i!</h1>
       <Row title="Popular Movies" getURL={apiClient.getMovieList} />
       <Row title="Popular Series" getURL={apiClient.getTVList} />
       <Row title="Family" getURL={apiClient.getFamilyMovies} />
