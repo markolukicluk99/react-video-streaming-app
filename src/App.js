fe3ce3625569a4a60f1f9dc3eeb4ebf9853c7754
Marko Lukić <lukic@netsearch.cz>
2021-04-09 19:46:25 +0200

    Introduce new paths & add padding

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+//Pages
 import Homepage from './pages/homepage';
-import Navigation from './components/Navigation'
+import Search from './pages/search';
+import MovieDetail from './pages/moviedetail'
+import Streaming from './pages/streaming'
+//Components
+import Navigation from './components/Navigation';
 
 import "./App.scss"
 
 function App() {
   return (
     <Router>
+
     <div className="App">
       <Navigation />
       <Route exact path="/" render={ () => <Redirect to="/homepage" /> }/>
       <Route path="/homepage" component={Homepage} />
+      <Route path="/search" component={Search} />
+      <Route path="/detail/:type/:id" component={MovieDetail} />
+      <Route path="/streaming" component={Streaming} />
       {/* <Route path="/streaming" component={StreamingPage} /> */}
     </div>
     </Router>
