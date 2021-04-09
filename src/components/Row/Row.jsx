528d9ab5a1b64acdbedd9a2a857eb9f4e0bb6458
Marko Lukić <lukic@netsearch.cz>
2021-04-09 19:49:14 +0200

    Update the whole concept & logic of loading the banners

--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -1,32 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import axios from '../../modules/axios';
+import React, { useState, useEffect, useContext } from "react";
+import axios from "../../modules/axios";
+import { useHistory } from "react-router-dom";
 
 import "./Row.scss";
 
 const base_url_images = "https://image.tmdb.org/t/p/w500/";
 
-function Row( { title, getURL } ) {
-    const [movies, setMovies] = useState([]);
+function Row({ title, getURL, isSeries }) {
+  let history = useHistory();
 
-    useEffect(() => {
-        async function getData() {
-            const request = await axios.get(getURL);
-            setMovies(request.data.results);
-            return request;
-        }
-        getData();
-    }, [getURL]);
+  const [movies, setMovies] = useState(null);
 
-    return (
-        <div className="wrapper">
-            <h2 className="title">{title}</h2>
-                <div className="rowPosters">
-                    {movies.map(movie => (
-                        <img key={movie.id} className="rowPoster" src={`${base_url_images}${movie.poster_path}`} alt={movie.title} />
-                    ))}
-                </div>
+  useEffect(() => {
+    const getData = async () => {
+      const request = await axios.get(getURL);
+      setMovies(request.data.results);
+      console.log(request.data);
+      if (request.data === null) {
+        console.warn("Error");
+      }
+    };
+    getData();
+  }, [getURL]);
+
+  const redirect = (id) => {
+    history.push(`/detail/${isSeries ? "tv" : "movie"}/${id}`);
+  };
+
+  return (
+    <div className="wrapper">
+      <h2 className="title">{title}</h2>
+      {movies ? (
+        <div className="rowPosters">
+          {movies.map((movie) => (
+            <img
+              onClick={() => redirect(movie.id)}
+              key={movie.id}
+              className="rowPoster"
+              src={`${base_url_images}${movie.poster_path}`}
+              alt={movie.title}
+            />
+          ))}
         </div>
-    )
+      ) : (
+        <div>
+          <h1>LOADING {/* SPINNER */}</h1>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Row
+export default Row;
