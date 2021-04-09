8977ef00284930ae6d7e37232f7df48e1f915f04
Marko Lukić <lukic@netsearch.cz>
2021-04-09 23:48:03 +0200

    Implement the logic for video streaming & design the outlay

--- src/pages/streaming.jsx
+++ src/pages/streaming.jsx
@@ -1,15 +1,19 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import shaka from "shaka-player";
 import { useHistory } from "react-router-dom";
+import { Alert } from 'react-bootstrap';
 
-function Streaming() {
-  const videoComponent = useRef();
+import './streaming.scss'
+
+function Streaming(props) {
+  console.log(props);
+  const videoEl = useRef();
   let history = useHistory();
+  const [alertShown, setAlertShown] = useState(true);
 
-  const onErrorEvent = () => {};
-  const onError = (error, player) => {
+  const onStreamLoadError = (error, player) => {
     player.destroy().then(() => {
-      const video = videoComponent.current;
+      const video = videoEl.current;
       if (video) {
         video.src =
           "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
@@ -17,29 +21,45 @@ function Streaming() {
       }
     });
   };
-  const closeDialog = () => {
-    history.push("/homepage");
-  };
+
 
   useEffect(() => {
-    const video = videoComponent.current;
+    const video = videoEl.current;
+    document.addEventListener("keydown", keyPress, false);
+
+    setTimeout(() => {
+      setAlertShown(false);
+    }, 5000)
+
+
+
     if (video) {
       const player = new shaka.Player(video);
-      player.addEventListener("error", onErrorEvent);
-
-      // Try to play the video
+      player.addEventListener("error", () => console.log('Error during playback.'));
       player
         .load("https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8")
-        .catch((err) => player && onError(err, player));
+
+        .catch((error) => player && onStreamLoadError(error, player));
     }
+    return () => {
+      document.removeEventListener("keydown", keyPress, false);
+    };
   }, []);
 
+
+  const keyPress = (e) => {
+    if(e.key === "Escape") {
+      history.goBack();
+    }
+  }
+
   return (
-    <div>
-      <p className="closeIcon" role="presentation" onClick={closeDialog}>
-        Close
-      </p>
-      <video ref={videoComponent} controls autoPlay className="assetPlayer" />
+    <div className="streamingWrapper">
+
+      <Alert variant="primary" className="alert" show={alertShown} >
+      Use the Escape key to close video preview.
+      </Alert>
+      <video ref={videoEl} className="video" controls autoPlay />
     </div>
   );
 }
