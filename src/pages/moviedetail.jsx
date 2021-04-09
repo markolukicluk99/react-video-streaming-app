df64f519b926b47d46cfeba2c196a21b85c80a66
Marko Lukić <lukic@netsearch.cz>
2021-04-09 23:47:23 +0200

    Introduce Movie & Series detail to the app

--- src/pages/moviedetail.jsx
+++ src/pages/moviedetail.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import axios from "../modules/axios";
+import { Container, Row, Col, Button, Spinner } from "react-bootstrap";
+
+import "./moviedetail.scss";
 
 const APIKEY = process.env.REACT_APP_API_KEY;
+const base_url_images = "https://image.tmdb.org/t/p/w500/";
 
 function MovieDetail() {
-    let history = useHistory();
+  let history = useHistory();
   let { type, id } = useParams();
 
   const [source, setSource] = useState(null);
@@ -22,27 +26,38 @@ function MovieDetail() {
   }, [getURL]);
 
   if (source === null) {
-    return <h1>LOADING</h1>;
+    return (
+      <Spinner animation="border" role="status">
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    );
   }
 
   const redirect = () => {
-    history.push(`/streaming`)
-  }
+    history.push(`/streaming`);
+  };
 
   return (
-    <div>
-
-        <h1 >
-        {type === 'movie' ? source.title : source.name}  
-        </h1>  
-        <p>
-        { source.overview }  
-        </p>
-        <button onClick={redirect}>
-            PLAY BUTTON
-        </button>
-
-      
+    <div className="movieDetailWrapper">
+      <Container>
+        <Row>
+          <Col sm={6}>
+            <h1 className="header">
+              {type === "movie" ? source.title : source.name}
+            </h1>
+            <p className="paragraph">{source.overview}</p>
+            <Button variant="primary" onClick={redirect}>
+              PLAY TRAILER
+            </Button>
+          </Col>
+          <Col sm={4}>
+            <img
+              src={`${base_url_images}${source.poster_path}`}
+              alt={type === "movie" ? source.title : source.name}
+            />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
