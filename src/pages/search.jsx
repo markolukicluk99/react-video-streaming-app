77497ba086f89af5a20a4b3b8fe8f8ee024852d0
Marko Lukić <lukic@netsearch.cz>
2021-04-09 19:50:41 +0200

    Prepare the Search component

---
+++ src/pages/search.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function search() {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <h2>potatoes</h2>
+        </div>
+    )
+}
+
+export default search
