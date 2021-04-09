0330cd7a9af73b76a0f52049d3cba4b219bcdf67
Marko Lukić <lukic@netsearch.cz>
2021-04-09 20:12:52 +0200

    Resolve visual issues with the homepage

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,17 +6,17 @@ function Navigation() {
   return (
     <div>
       <Navbar collapseOnSelect fixed="top" expand="sm" bg="dark" variant="dark">
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            {/* <Navbar.Brand href="/homepage">24i Interview</Navbar.Brand> */}
-            <Nav>
-              <Nav.Link href="/homepage">Home</Nav.Link>
-            </Nav>
-            {/* <Form inline>
+        <Navbar.Brand href="/homepage">24i Interview</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/homepage">Home</Nav.Link>
+          </Nav>
+          {/* <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-info">Search</Button>
         </Form> */}
-          </Navbar.Collapse>
+        </Navbar.Collapse>
       </Navbar>
     </div>
   );
